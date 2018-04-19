e9b9d3570d39257a4ea8bf3136b352b4ad286e01
Holger_Tidemand <tidemand.holger@gmail.com>
2018-04-19 14:10:33 +0200

    followed the guide

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return ([
+        {title: 'Harry Potter', pages: 101},
+        {title: 'Byggare bob', pages: 22},
+        {title: 'Harry tråkmåns', pages: 12},
+        {title: 'Bla book', pages: 12}
+    ]);
+}
